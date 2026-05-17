c64d5caaeab1d51af689322243f663b4be26cccd
obiettivomaturita <obiettivomaturita@gmail.com>
2026-05-17 21:23:33 +0200

    vecchi file eliminati

--- Tracce-INFO/traccia-straordinaria-2019-ospedale/punto 3 problema/db_connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$connection=new mysqli("localhost","root","nuova_password","Ospedale_2019");
-if($connection->connect_error){
-    die("Errore di connessione". $connection->connect_error);
-}
-?>
-
